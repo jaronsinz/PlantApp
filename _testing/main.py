9f8b1411000561144d29c5491fca0dd81f827619
jaronsinz <jaron.sinz@gmx.de>
2024-05-27 09:32:34 +0200

    first kivy test

--- _testing/main.py
+++ _testing/main.py
@@ -1,24 +1,11 @@
+import kivy
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.button import Button
 from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
-from kivy.uix.pagelayout import PageLayout
-from kivy.uix.gridlayout import GridLayout
-
-
-class HomeScreen(GridLayout): 
-    pass
-    
 
 class PlantApp(App):
     def build(self):
-        root = HomeScreen()
+        return Label(text="PlantApp")
     
-    def say_hello(self):
-        print("Hello world")
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     PlantApp().run()
-    
+
